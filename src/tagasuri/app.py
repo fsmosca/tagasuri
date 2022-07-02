6cf1f1d6ffb63d8cf2d654ff92d9d5ac702a8f99
fsmosca <fsmosca@users.noreply.github.com>
2022-07-02 17:34:34 +0800

    Update app.py

--- src/tagasuri/app.py
+++ src/tagasuri/app.py
@@ -3,13 +3,13 @@ from tagasuri.epd import EpdTest
 
 def epd_test(
         enginepath, inputfile, outputfile, masterfile: str = 'master.csv',
-        movetime: float = 1.0, engineoptions: str = None):
+        movetime: float = 1.0, engineoptions: str = None, workers: int = 1):
 
     a = EpdTest(
         enginepath, inputfile, outputfile, masterfile=masterfile,
-        movetime=movetime, engineoptions=engineoptions)
+        movetime=movetime, engineoptions=engineoptions, workers=workers)
 
-    df = a.run()
+    df = a.start()
     a.save_to_master(df)
     dfs = a.get_summary()
     dfs.columns = ['Name', 'Total', 'Correct', 'Pct', 'Time', 'EPDFile']
